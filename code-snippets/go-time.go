cb371254c3e7904b08e4dacf3fac7078620359a9
Omar Eid <oeid@cognitivescale.com>
2020-09-20 10:59:18 -0500

    Adding a yaml serialization / deserialization example

--- code-snippets/go-time.go
+++ code-snippets/go-time.go
@@ -5,9 +5,12 @@ import "fmt"
 
 func main() {
   // fmt.Printf(time.Date(0,0,0,0,0,0))
-  fmt.Printf(time.Unix(0,0).UTC().String())
+  fmt.Println(time.Unix(0,0).UTC().String())
   fmt.Println(onSameDay(time.Date(2020,1,1,0,0,0,0,time.UTC), time.Date(2020,1,1,0,0,1,0,time.UTC)))
   fmt.Println(onSameDay(time.Date(2020,1,1,0,0,0,0,time.UTC), time.Date(2020,1,2,0,0,0,0,time.UTC)))
+  // https://www.ribice.ba/golang-enums/
+  fmt.Println(time.Date(2020,8,21,0,0,0,0,time.UTC).Weekday() == time.Friday)
+  fmt.Println(time.Date(2020,8,24,0,0,0,0,time.UTC).Weekday() == time.Monday)
 }
 
 func onSameDay(timeA time.Time, timeB time.Time) bool {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.14
 
-require github.com/google/uuid v1.1.1
+require (
+  github.com/google/uuid v1.1.1
+  gopkg.in/yaml.v2 v2.2.7
+)
